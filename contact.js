04a93455dcd4ff987dbe3b8feaa792acf7843e2d
Oscar-Aksanti <aksantioscar@gmail.com>
2018-12-14 11:28:06 +0200

    Coding the main loop

--- contact.js
+++ contact.js
@@ -20,5 +20,34 @@ contacts.push(carole);
 contacts.push(melodie);
 
 console.log("Bienvenue dans le gestionnaire des contacts !");
+var choix;
+while (choix !== "0") {
+    console.log("1 : Lister les contacts");
+    console.log("2 : Ajouter un contact");
+    console.log("0 : Quitter");
+    var choix = prompt("Choisissez une option :");
 
+    switch (choix) {
+    case "1":
+        console.log("Voici la liste de tous vos contacts :");
+        // Avec une boucle for
+        for (var i = 0; i < contacts.length; i++) {
+            console.log(contacts[i].decrire());
+        }
+        // Avec une boucle foreach
+        /*contacts.forEach(function (contact) {
+            console.log(contact.decrire());
+        });*/
+        break;
+    case "2":
+        var nom = prompt("Entrez le nom du nouveau contact :");
+        var prenom = prompt("Entrez le prénom du nouveau contact :");
+        var contact = Object.create(Contact);
+        contact.init(nom, prenom);
+        contacts.push(contact);
+        console.log("Le nouveau contact a été ajouté");
+        break;
+    }
+    console.log(); // Passe une ligne
+}
 console.log("Au revoir !");
